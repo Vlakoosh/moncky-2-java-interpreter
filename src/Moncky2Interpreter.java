436c3a6dff1b10126c2f9689dd124e434cdf9a08
vlakoosh <vlakoosh@gmail.com>
2024-03-20 23:37:21 +0100

    added support for labels using ":" inside the interpreter
    Signed-off-by: Szymon Walkusz <szymon.walkusz@student.kdg.be>

--- src/Moncky2Interpreter.java
+++ src/Moncky2Interpreter.java
@@ -8,6 +8,7 @@ public class Moncky2Interpreter {
     private short[] register = new short[16];
     private short[] memory = new short[65536];
     private short ALU = 0;
+    private String[] commands;
     public ArrayList<String> compiledBinaryCommands = new ArrayList<String>();
 
 
@@ -28,8 +29,7 @@ public class Moncky2Interpreter {
     }
 
     public void interpretCode(String moncky2Code) {
-        String[] commands = moncky2Code.split("\n");
-
+        commands = moncky2Code.split("\n");
         int commandLine = 0;
         while (true) {
             int commandResult = executeCommand(commands[commandLine]);
@@ -79,7 +79,26 @@ public class Moncky2Interpreter {
             else registerNumber = Integer.parseInt(commandParts[1].substring(1, 2));
 
             //save the immediate value from command
-            immediateValue = Short.parseShort(commandParts[2]);
+            if (commandParts[2].charAt(0) == ':'){
+                String label = commandParts[2];
+                immediateValue = -1;
+                int commandLine = 0;
+                boolean found = false;
+                for (int i = 0; i < commands.length; i ++) {
+                    if (commands[i].strip().equals(label)){
+                        immediateValue = (short) (i + 1);
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    throw new RuntimeException("label not found in code");
+                }
+            }
+            else {
+                immediateValue = Short.parseShort(commandParts[2]);
+            }
+
             register[registerNumber] = immediateValue;
             //add command to compiler
             compiledBinaryCommands.add("0001" + NumberConverter.decimalToBinaryString(immediateValue, 8) + NumberConverter.decimalToBinaryString(registerNumber, 4));
@@ -141,172 +160,175 @@ public class Moncky2Interpreter {
         }
 
 
+        if (commandParts.length > 1) {
+            //SETUP FOR ALU OPERATIONS
+            int firstRegisterNumber;
+            int secondRegisterNumber;
 
-        //SETUP FOR ALU OPERATIONS
-        int firstRegisterNumber;
-        int secondRegisterNumber;
+            //check if register number is 1 or 2 digit (0-15)
+            if (commandParts[1].length() == 4)
+                firstRegisterNumber = Integer.parseInt(commandParts[1].substring(1, 3));
+            else firstRegisterNumber = Integer.parseInt(commandParts[1].substring(1, 2));
 
-        //check if register number is 1 or 2 digit (0-15)
-        if (commandParts[1].length() == 4)
-            firstRegisterNumber = Integer.parseInt(commandParts[1].substring(1, 3));
-        else firstRegisterNumber = Integer.parseInt(commandParts[1].substring(1, 2));
+            //check if register number is 1 or 2 digit (0-15)
+            if (commandParts[2].length() == 3)
+                secondRegisterNumber = Integer.parseInt(commandParts[2].substring(1, 3));
+            else secondRegisterNumber = Integer.parseInt(commandParts[2].substring(1, 2));
 
-        //check if register number is 1 or 2 digit (0-15)
-        if (commandParts[2].length() == 3)
-            secondRegisterNumber = Integer.parseInt(commandParts[2].substring(1, 3));
-        else secondRegisterNumber = Integer.parseInt(commandParts[2].substring(1, 2));
+            //ALU OPERATIONS
+            if (commandParts[0].equalsIgnoreCase("nop")) {
+                //stupid command that does nothing
 
-        //ALU OPERATIONS
-        if (commandParts[0].equalsIgnoreCase("nop")) {
-            //stupid command that does nothing
+                //save ALU result
+                ALU = 0;
 
-            //save ALU result
-            ALU = 0;
+                //add the command to compiler
+                compiledBinaryCommands.add("01000000" + NumberConverter.decimalToBinaryString(firstRegisterNumber, 4) + NumberConverter.decimalToBinaryString(secondRegisterNumber, 4));
 
-            //add the command to compiler
-            compiledBinaryCommands.add("01000000" + NumberConverter.decimalToBinaryString(firstRegisterNumber, 4) + NumberConverter.decimalToBinaryString(secondRegisterNumber, 4));
+                return 0;
+            }
+            if (commandParts[0].equalsIgnoreCase("or")) {
+                //set reg1 to 1 if either register is true
+                if (register[firstRegisterNumber] == 1 || register[secondRegisterNumber] == 1)
+                    register[firstRegisterNumber] = (short) 1;
+                else register[firstRegisterNumber] = (short) 0;
 
-            return 0;
-        }
-        if (commandParts[0].equalsIgnoreCase("or")) {
-            //set reg1 to 1 if either register is true
-            if (register[firstRegisterNumber] == 1 || register[secondRegisterNumber] == 1) register[firstRegisterNumber] = (short) 1;
-            else register[firstRegisterNumber] = (short) 0;
+                //save ALU result
+                ALU = register[firstRegisterNumber];
 
-            //save ALU result
-            ALU = register[firstRegisterNumber];
+                //add the command to compiler
+                compiledBinaryCommands.add("01000001" + NumberConverter.decimalToBinaryString(firstRegisterNumber, 4) + NumberConverter.decimalToBinaryString(secondRegisterNumber, 4));
 
-            //add the command to compiler
-            compiledBinaryCommands.add("01000001" + NumberConverter.decimalToBinaryString(firstRegisterNumber, 4) + NumberConverter.decimalToBinaryString(secondRegisterNumber, 4));
+                return 0;
+            }
+            if (commandParts[0].equalsIgnoreCase("and")) {
+                //set reg1 to 1 if both registers are true
+                if (register[firstRegisterNumber] == 1 && register[secondRegisterNumber] == 1)
+                    register[firstRegisterNumber] = (short) 1;
+                else register[firstRegisterNumber] = (short) 0;
 
-            return 0;
-        }
-        if (commandParts[0].equalsIgnoreCase("and")) {
-            //set reg1 to 1 if both registers are true
-            if (register[firstRegisterNumber] == 1 && register[secondRegisterNumber] == 1) register[firstRegisterNumber] = (short) 1;
-            else register[firstRegisterNumber] = (short) 0;
+                //save ALU result
+                ALU = register[firstRegisterNumber];
 
-            //save ALU result
-            ALU = register[firstRegisterNumber];
+                //add the command to compiler
+                compiledBinaryCommands.add("010000010" + NumberConverter.decimalToBinaryString(firstRegisterNumber, 4) + NumberConverter.decimalToBinaryString(secondRegisterNumber, 4));
 
-            //add the command to compiler
-            compiledBinaryCommands.add("010000010" + NumberConverter.decimalToBinaryString(firstRegisterNumber, 4) + NumberConverter.decimalToBinaryString(secondRegisterNumber, 4));
+                return 0;
+            }
+            if (commandParts[0].equalsIgnoreCase("xor")) {
+                //set reg1 to 1 if either register is true, but not both
+                if ((register[firstRegisterNumber] == 1 || register[secondRegisterNumber] == 1) && register[firstRegisterNumber] != register[secondRegisterNumber])
+                    register[firstRegisterNumber] = (short) 1;
+                else register[firstRegisterNumber] = (short) 0;
 
-            return 0;
-        }
-        if (commandParts[0].equalsIgnoreCase("xor")) {
-            //set reg1 to 1 if either register is true, but not both
-            if ((register[firstRegisterNumber] == 1 || register[secondRegisterNumber] == 1) && register[firstRegisterNumber] != register[secondRegisterNumber]) register[firstRegisterNumber] = (short) 1;
-            else register[firstRegisterNumber] = (short) 0;
+                //save ALU result
+                ALU = register[firstRegisterNumber];
 
-            //save ALU result
-            ALU = register[firstRegisterNumber];
+                //add the command to compiler
+                compiledBinaryCommands.add("01000011" + NumberConverter.decimalToBinaryString(firstRegisterNumber, 4) + NumberConverter.decimalToBinaryString(secondRegisterNumber, 4));
 
-            //add the command to compiler
-            compiledBinaryCommands.add("01000011" + NumberConverter.decimalToBinaryString(firstRegisterNumber, 4) + NumberConverter.decimalToBinaryString(secondRegisterNumber, 4));
-
-            return 0;
-        }
-        if (commandParts[0].equalsIgnoreCase("add")) {
-            //add the 2 register values and store them in the first register
-            ALU = (short) (register[firstRegisterNumber] + register[secondRegisterNumber]);
-            register[firstRegisterNumber] = ALU;
-
-            //add the command to compiler
-            compiledBinaryCommands.add("01000100" + NumberConverter.decimalToBinaryString(firstRegisterNumber, 4) + NumberConverter.decimalToBinaryString(secondRegisterNumber, 4));
-
-            return 0;
-        }
-        if (commandParts[0].equalsIgnoreCase("sub")) {
-            //subtract the 2 register values and store them in the first register
-            ALU = (short) (register[firstRegisterNumber] - register[secondRegisterNumber]);
-            register[firstRegisterNumber] = ALU;
-
-            //add the command to compiler
-            compiledBinaryCommands.add("01000101" + NumberConverter.decimalToBinaryString(firstRegisterNumber, 4) + NumberConverter.decimalToBinaryString(secondRegisterNumber, 4));
-
-            return 0;
-        }
-        if (commandParts[0].equalsIgnoreCase("shl")) {
-            //shift all bits left by a specified number of bits
-            //the right-most (low-order) bit is turned into a 0
-            //the left-most (high-order) bit is discarded
-            for (int i = 0; i < register[secondRegisterNumber]; i++){
-                String binary = NumberConverter.decimalToBinaryString(register[firstRegisterNumber], 16);
-                binary = binary.substring(1) + "0";
-                ALU = (short) NumberConverter.binaryStringToDecimal(binary);
+                return 0;
+            }
+            if (commandParts[0].equalsIgnoreCase("add")) {
+                //add the 2 register values and store them in the first register
+                ALU = (short) (register[firstRegisterNumber] + register[secondRegisterNumber]);
                 register[firstRegisterNumber] = ALU;
+
+                //add the command to compiler
+                compiledBinaryCommands.add("01000100" + NumberConverter.decimalToBinaryString(firstRegisterNumber, 4) + NumberConverter.decimalToBinaryString(secondRegisterNumber, 4));
+
+                return 0;
             }
-
-            //add the command to compiler
-            compiledBinaryCommands.add("01000110" + NumberConverter.decimalToBinaryString(firstRegisterNumber, 4) + NumberConverter.decimalToBinaryString(secondRegisterNumber, 4));
-
-            return 0;
-        }
-        if (commandParts[0].equalsIgnoreCase("shr")) {
-            //shift all bits right by a specified number of bits
-            //the right-most (low-order) bit is discarded
-            //the left-most (high-order) bit is turned into a 0
-            for (int i = 0; i < register[secondRegisterNumber]; i++){
-                String binary = NumberConverter.decimalToBinaryString(register[firstRegisterNumber], 16);
-                binary = "0" + binary.substring(0, binary.length()-1);
-                ALU = (short) NumberConverter.binaryStringToDecimal(binary);
+            if (commandParts[0].equalsIgnoreCase("sub")) {
+                //subtract the 2 register values and store them in the first register
+                ALU = (short) (register[firstRegisterNumber] - register[secondRegisterNumber]);
                 register[firstRegisterNumber] = ALU;
+
+                //add the command to compiler
+                compiledBinaryCommands.add("01000101" + NumberConverter.decimalToBinaryString(firstRegisterNumber, 4) + NumberConverter.decimalToBinaryString(secondRegisterNumber, 4));
+
+                return 0;
             }
+            if (commandParts[0].equalsIgnoreCase("shl")) {
+                //shift all bits left by a specified number of bits
+                //the right-most (low-order) bit is turned into a 0
+                //the left-most (high-order) bit is discarded
+                for (int i = 0; i < register[secondRegisterNumber]; i++) {
+                    String binary = NumberConverter.decimalToBinaryString(register[firstRegisterNumber], 16);
+                    binary = binary.substring(1) + "0";
+                    ALU = (short) NumberConverter.binaryStringToDecimal(binary);
+                    register[firstRegisterNumber] = ALU;
+                }
 
-            //add the command to compiler
-            compiledBinaryCommands.add("01000111" + NumberConverter.decimalToBinaryString(firstRegisterNumber, 4) + NumberConverter.decimalToBinaryString(secondRegisterNumber, 4));
+                //add the command to compiler
+                compiledBinaryCommands.add("01000110" + NumberConverter.decimalToBinaryString(firstRegisterNumber, 4) + NumberConverter.decimalToBinaryString(secondRegisterNumber, 4));
 
-            return 0;
-        }
-        if (commandParts[0].equalsIgnoreCase("ashr")) {
-            //shift all bits right by a specified number of bits (and keep the sign bit)
-            //the right-most (low-order) bit is discarded
-            //the left-most (high-order) bit is copied
-            for (int i = 0; i < register[secondRegisterNumber]; i++){
-                String binary = NumberConverter.decimalToBinaryString(register[firstRegisterNumber], 16);
-                binary = binary.charAt(0) + binary.substring(0, binary.length()-1);
-                ALU = register[firstRegisterNumber] = (short) NumberConverter.binaryStringToDecimal(binary);
-                register[firstRegisterNumber] = ALU;
+                return 0;
             }
+            if (commandParts[0].equalsIgnoreCase("shr")) {
+                //shift all bits right by a specified number of bits
+                //the right-most (low-order) bit is discarded
+                //the left-most (high-order) bit is turned into a 0
+                for (int i = 0; i < register[secondRegisterNumber]; i++) {
+                    String binary = NumberConverter.decimalToBinaryString(register[firstRegisterNumber], 16);
+                    binary = "0" + binary.substring(0, binary.length() - 1);
+                    ALU = (short) NumberConverter.binaryStringToDecimal(binary);
+                    register[firstRegisterNumber] = ALU;
+                }
 
-            //add the command to compiler
-            compiledBinaryCommands.add("01001001" + NumberConverter.decimalToBinaryString(firstRegisterNumber, 4) + NumberConverter.decimalToBinaryString(secondRegisterNumber, 4));
+                //add the command to compiler
+                compiledBinaryCommands.add("01000111" + NumberConverter.decimalToBinaryString(firstRegisterNumber, 4) + NumberConverter.decimalToBinaryString(secondRegisterNumber, 4));
 
-            return 0;
-        }
-        //TODO: check if this is right as I am *NOT* sure
-        if (commandParts[0].equalsIgnoreCase("not")) {
-            //1-complement
-            String bitValue = NumberConverter.decimalToBinaryString(register[secondRegisterNumber], 16);
-            String reverseBitValue = "";
-            //reverse the bit value of bitValue
-            for (int i = 0; i < bitValue.length(); i++){
-                if (bitValue.charAt(i) == '0') reverseBitValue = reverseBitValue + "1";
-                else reverseBitValue = reverseBitValue + "0";
+                return 0;
             }
+            if (commandParts[0].equalsIgnoreCase("ashr")) {
+                //shift all bits right by a specified number of bits (and keep the sign bit)
+                //the right-most (low-order) bit is discarded
+                //the left-most (high-order) bit is copied
+                for (int i = 0; i < register[secondRegisterNumber]; i++) {
+                    String binary = NumberConverter.decimalToBinaryString(register[firstRegisterNumber], 16);
+                    binary = binary.charAt(0) + binary.substring(0, binary.length() - 1);
+                    ALU = register[firstRegisterNumber] = (short) NumberConverter.binaryStringToDecimal(binary);
+                    register[firstRegisterNumber] = ALU;
+                }
 
-            register[firstRegisterNumber] = (short) NumberConverter.binaryStringToDecimal(reverseBitValue);
+                //add the command to compiler
+                compiledBinaryCommands.add("01001001" + NumberConverter.decimalToBinaryString(firstRegisterNumber, 4) + NumberConverter.decimalToBinaryString(secondRegisterNumber, 4));
 
-            //add the command to compiler
-            compiledBinaryCommands.add("01001001" + NumberConverter.decimalToBinaryString(firstRegisterNumber, 4) + NumberConverter.decimalToBinaryString(secondRegisterNumber, 4));
+                return 0;
+            }
+            //TODO: check if this is right as I am *NOT* sure
+            if (commandParts[0].equalsIgnoreCase("not")) {
+                //1-complement
+                String bitValue = NumberConverter.decimalToBinaryString(register[secondRegisterNumber], 16);
+                String reverseBitValue = "";
+                //reverse the bit value of bitValue
+                for (int i = 0; i < bitValue.length(); i++) {
+                    if (bitValue.charAt(i) == '0') reverseBitValue = reverseBitValue + "1";
+                    else reverseBitValue = reverseBitValue + "0";
+                }
 
-            return 0;
-        }
-        //TODO neg
-        if (commandParts[0].equalsIgnoreCase("neg")) {
+                register[firstRegisterNumber] = (short) NumberConverter.binaryStringToDecimal(reverseBitValue);
 
-            //2-complement
-            register[firstRegisterNumber] = 0;
+                //add the command to compiler
+                compiledBinaryCommands.add("01001001" + NumberConverter.decimalToBinaryString(firstRegisterNumber, 4) + NumberConverter.decimalToBinaryString(secondRegisterNumber, 4));
 
-            //add the command to compiler
-            compiledBinaryCommands.add("01001010" + NumberConverter.decimalToBinaryString(firstRegisterNumber, 4) + NumberConverter.decimalToBinaryString(secondRegisterNumber, 4));
+                return 0;
+            }
+            //TODO neg
+            if (commandParts[0].equalsIgnoreCase("neg")) {
 
-            return 0;
-        }
+                //2-complement
+                register[firstRegisterNumber] = 0;
 
+                //add the command to compiler
+                compiledBinaryCommands.add("01001010" + NumberConverter.decimalToBinaryString(firstRegisterNumber, 4) + NumberConverter.decimalToBinaryString(secondRegisterNumber, 4));
+
+                return 0;
+            }
+        }//ALU
 
         return 0;
-    }
+    }//executeCommand
 }
 
